9970c71791af92900b1293f5fcad6df260e6d171
SevvalAcet <83153151+SevvalAcet@users.noreply.github.com>
2022-10-13 22:04:41 +0300

    add crud operations

---
+++ Business/Abstract/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+
+namespace Business.Abstract
+{
+    public interface IUserService
+    {
+        IDataResult<List<User>> GetAll();
+        IResult Add(User user);
+        IResult Delete(User user);
+        IResult Update(User user);
+    }
+}
--- DataAccess/Abstract/ICustomerDal.cs
+++ DataAccess/Abstract/ICustomerDal.cs
@@ -3,7 +3,7 @@ using Entities.Concrete;
 
 namespace DataAccess.Abstract
 {
-    public interface IUserDal:IEntityRepository<User>
+    public interface ICustomerDal:IEntityRepository<Customer>
     {
     }
 }
