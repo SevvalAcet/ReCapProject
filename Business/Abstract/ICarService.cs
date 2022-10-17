cf29867bdaa1a1e9c47bd6d2843e4c4b4b0cf97e
SevvalAcet <83153151+SevvalAcet@users.noreply.github.com>
2022-10-17 20:38:44 +0300

    added webAPI

--- Business/Abstract/ICarService.cs
+++ Business/Abstract/ICarService.cs
@@ -7,11 +7,12 @@ namespace Business.Abstract
     public interface ICarService
     {
         IDataResult<List<Car>> GetAll();
-        IDataResult<List<Car>> GetCarsBrandId(int brandId);
+        IDataResult<List<Car>> GetCarsByBrandId(int brandId);
         IDataResult<List<Car>> GetCarsByColorId(int colorId);
         IResult Add(Car car);
         IResult Delete(Car car);
-        IDataResult<List<CarDetailDto>> GetCarDetails();
+        IResult Update(Car car);
+        IDataResult<List<CarDetailDto>> GetCarsDetails();
 
     }
 }
